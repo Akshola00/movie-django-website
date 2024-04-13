c10ee31923eaffec54ee3c9071769de09ad480e7
Akshola00 <akinniyishola07@gmail.com>
2024-04-13 22:03:09 +0100

    modified:   db.sqlite3 	modified:   mysite/__pycache__/urls.cpython-311.pyc 	modified:   mysite/urls.py 	modified:   newapp/__pycache__/models.cpython-311.pyc 	new file:   newapp/__pycache__/views.cpython-311.pyc 	modified:   newapp/models.py 	new file:   newapp/templates/newapp/movie_list.html 	new file:   newapp/urls.py 	modified:   newapp/views.py

--- newapp/models.py
+++ newapp/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class movies(models.Model):
     name = models.CharField(max_length=200)
     rating = models.FloatField()
+
+    def __str__(self) -> str:
+        return self.name
--- newapp/views.py
+++ newapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from .models import movies
+from django.core.paginator import Paginator
 # Create your views here.
+
+def movie_list(request):
+    movies_objects = movies.objects.all()
+    paginator = Paginator(movies_objects, 3)
+    context = {
+        'movies_objects':movies_objects
+    }
+    return render(request, 'newapp/movie_list.html', context)
