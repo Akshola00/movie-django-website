ee1f54a7f38973b0d633e505a7cc0855bcc54c03
Akshola00 <akinniyishola07@gmail.com>
2024-04-16 23:07:04 +0100

    modified:   newapp/__pycache__/views.cpython-311.pyc 	modified:   newapp/templates/newapp/movie_list.html 	modified:   newapp/views.py

--- newapp/views.py
+++ newapp/views.py
@@ -5,6 +5,11 @@ from django.core.paginator import Paginator
 
 def movie_list(request):
     movies_objects = movies.objects.all()
+
+    movie_name = request.GET.get('movie_name')
+
+    if movie_name != '' and movie_name is not None:
+        movies_objects = movies_objects.filter(name__icontains=movie_name)
     paginator = Paginator(movies_objects, 3)
     page = request.GET.get('page')
     movies_objects = paginator.get_page(page)
